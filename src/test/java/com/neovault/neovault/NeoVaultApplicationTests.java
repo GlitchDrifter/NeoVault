654e5884a9d46494c00fcf3e8ffb213fa7f8e313
aylatshjhb024 <aylatshjhb024@student.wethinkcode.co.za>
2025-05-01 10:13:39 +0200

    first commit - boilerplate code

---
+++ src/test/java/com/neovault/neovault/NeoVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neovault.neovault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
