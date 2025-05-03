cb67d9f47c1452c3fab5ad46af843c5288ea08e1
aylatshjhb024 <aylatshjhb024@student.wethinkcode.co.za>
2025-05-03 23:12:34 +0200

    add mariadb dependency

---
+++ src/main/java/com/neovault/NeoVaultApplication.java
@@ -0,0 +1,13 @@
+package com.neovault;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class NeoVaultApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NeoVaultApplication.class, args);
+  }
+
+}
